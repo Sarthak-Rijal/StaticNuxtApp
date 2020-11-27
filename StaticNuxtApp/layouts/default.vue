2269226e02cf34ee1827b7b96b885ee425b4a2d1
Srijal <rijal.sarthak1@hotmail.com>
2020-11-27 09:47:27 -0500

    Basic site

---
+++ StaticNuxtApp/layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  
+  <div>
+    <div class="container font-sans text-black">
+      
+      <div class="flex justify-between items-center">
+        <h1><nuxt-link to="/">My Awesome Demo</nuxt-link></h1>
+        
+        <div class="flex">
+          <h1><nuxt-link to="/about">About</nuxt-link></h1>
+          <h1><nuxt-link to="/team">Team</nuxt-link></h1>
+        </div>
+
+      </div>
+    </div>
+
+    <Nuxt />
+  </div>
+
+  
+
+</template>
+
+<style>
+
+
+</style>
---
+++ StaticNuxtApp/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <div>
+    <ul>
+      <li><nuxt-link to="/blog/blog-post-1">Blog Post 1</nuxt-link></li>
+      <li><nuxt-link to="/blog/blog-post-1">Blog Post 1</nuxt-link></li>
+      <li><nuxt-link to="/blog/blog-post-1">Blog Post 1</nuxt-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+ 
+</style>
