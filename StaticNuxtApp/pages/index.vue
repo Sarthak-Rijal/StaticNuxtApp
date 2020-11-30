ec41cce316bafa81aa73f7c2325dfa8d61f7793b
Srijal <rijal.sarthak1@hotmail.com>
2020-11-30 11:37:53 -0500

    Made a mountains Page, Dad Joke did not work

--- StaticNuxtApp/pages/index.vue
+++ StaticNuxtApp/pages/index.vue
@@ -1,19 +1,29 @@
 <template>
-
-  <div>
-    <ul>
-      <li><nuxt-link to="/blog/blog-post-1">Blog Post 1</nuxt-link></li>
-      <li><nuxt-link to="/blog/blog-post-1">Blog Post 1</nuxt-link></li>
-      <li><nuxt-link to="/blog/blog-post-1">Blog Post 1</nuxt-link></li>
+  <p v-if="$fetchState.pending">Fetching Mountains...</p>
+  <p v-else-if="$fetchState.error">An error occurred :(</p>
+  <div v-else>
+    <h1>Nuxt Mountains</h1>
+    <ul v-for = "element of data">
+      <img v-bind:src = "element.image"/>
+      <li>{{element.title}} - {{element.height}}</li>
+      <br>
+      <li > {{element.description}}</li>
+      <br>
+      
     </ul>
+    <button @click="$fetch">Refresh</button>
   </div>
 </template>
 
 <script>
-export default {}
+  export default {
+    data() {
+      return {
+        data: []
+      }
+    },
+    async fetch() {
+      this.data = await fetch('https://api.nuxtjs.dev/mountains').then(res => res.json())
+    }
+  }
 </script>
-
-<style>
-
- 
-</style>
